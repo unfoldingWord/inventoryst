15472447c2f9dd531b9d14d93c6c31a8f44c57ff
Jakob Aleksandrovich <jakobaleksandrovich@gmail.com>
2023-10-18 17:00:16 +0200

    Various changes - Reformatted header warning - Started logging - Now only writes new file when new contents have changed.

--- platform/Platform.py
+++ platform/Platform.py
@@ -1,13 +1,19 @@
 import os
 import requests
 import datetime
+import hashlib
+import logging
 
 
 class Platform:
     def __init__(self):
         self._now = datetime.datetime.now(tz=datetime.timezone.utc)
 
-    def _get_json_from_url(self, url, authorization):
+        # Init logging
+        self._logger = logging.getLogger()
+
+    @staticmethod
+    def _get_json_from_url(url, authorization):
         # Basic headers
         req_headers = {
             'User-Agent': 'Inventoryst/1.0; https://github.com/unfoldingWord/inventoryst'
@@ -41,11 +47,11 @@ class Platform:
 
     def _get_header_warning(self):
         str_message = (">[!warning] Important notice: this page is automatically generated\n"
-                       ">Please be aware that this page is automatically generated and maintained by \
-                       [Inventoryst](https://www.github.com/unfoldingword/inventoryst). "
+                       ">Please be aware that this page is automatically generated and maintained by "
+                       "[Inventoryst](https://www.github.com/unfoldingword/inventoryst). "
                        "Any manual changes made to the content and layout will be overwritten during the next update. "
-                       "If you have specific information or customization needs, please contact your \
-                       System Administrator.\n"
+                       "If you have specific information or customization needs, please contact your "
+                       "System Administrator.\n"
                        )
 
         return str_message
@@ -54,24 +60,55 @@ class Platform:
         base_path = self._get_output_dir()
 
         if not os.path.exists(base_path):
-            raise FileExistsError(f"The directory '{base_path}' does not exist. Exiting.")
+            raise FileExistsError(f"The output directory '{base_path}' does not exist. Exiting.")
 
         for page in inventory:
+            self._logger.debug("Page: " + page)
+
             # Create path if it does not exist
             path = "/".join(page.split('/')[:-1])
             if not os.path.exists(base_path + "/" + path):
                 os.makedirs(base_path + "/" + path)
 
-            with open(base_path + "/" + page, 'w') as md_file:
-                # Last updated
-                str_date = datetime.datetime.utcnow().strftime("%B %d %Y, %I:%M %p")
-                md_file.write("*Last updated: " + str_date + "*\n")
+            # Prep page content
+            lst_page_content = list()
+            lst_page_content.append(self._get_header_warning() + "\n")
+            lst_page_content.append("\n".join(inventory[page]))
+            new_content = "".join(lst_page_content)
 
-                # Add generic warning
-                md_file.write(self._get_header_warning() + "\n")
+            # Generate hash for new content
+            hash_new = hashlib.sha256(new_content.encode()).hexdigest()
+            self._logger.debug("New hash: " + hash_new)
 
-                str_content = "\n".join(inventory[page])
-                md_file.write(str_content)
+            f_path = base_path + "/" + page
+            with open(f_path, 'r') as file:
+                lst_old_content = list()
+
+                # skip line with 'Last updated' statement (because it always changes)
+                next(file)
+
+                # Get all lines and collate them together
+                for line in file:
+                    lst_old_content.append(line)
+
+                old_content = "".join(lst_old_content)
+
+                # Generate hash for existing content
+                hash_old = hashlib.sha256(old_content.encode()).hexdigest()
+
+                self._logger.debug("Old hash: " + hash_old)
+                file.close()
+
+            if not hash_old == hash_new:
+                self._logger.info(f"Page content for '{page}' changed. Updating...")
+                with open(base_path + "/" + page, 'w') as md_file:
+                    # Last updated
+                    str_date = datetime.datetime.utcnow().strftime("%B %d %Y, %I:%M %p")
+                    md_file.write("*Last updated: " + str_date + "*\n")
+
+                    md_file.write(new_content)
+            else:
+                self._logger.debug(f"No changes for page '{page}'")
 
     def _build_content(self):
         raise NotImplementedError("You must override _build_content in your child class")
