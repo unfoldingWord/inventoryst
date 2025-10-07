f844c3d41195beb1af778bc0409eaa0561c47c3a
yakob-aleksandrovich <jakobaleksandrovich@gmail.com>
2025-10-07 13:58:14 +0200

    Enhancement: MySQL Platform now supports multiple hosts.

--- platforms/MySQL.py
+++ platforms/MySQL.py
@@ -11,43 +11,53 @@ class MySQL(Platform):
 
         self.__config = self.load_config('mysql')
 
-        self.db = mysql.connector.connect(
-            host=self.__config['host'],
-            user=self.__config['user'],
-            password=self.__config['password'],
-            ssl_ca=self.__config['ssl_ca_file'],
-            ssl_verify_cert=True
+    def __connect(self, host):
+        host_config = self.__config[host]
+
+        conn = mysql.connector.connect(
+            host=host_config['host'],
+            user=host_config['user'],
+            password=host_config['password'],
+            ssl_ca=host_config['ssl_ca_file'],
+            ssl_verify_cert=True,
+            collation=host_config['collation']
         )
 
-    def __list_users(self):
+        return conn
+
+    def __list_users(self, host):
         dict_users = dict()
 
         query_users = "SELECT `user`, `host` FROM `mysql`.`user`"
 
-        with self.db.cursor() as cursor:
+        db_conn = self.__connect(host)
+
+        with db_conn.cursor() as cursor:
             cursor.execute(query_users)
 
             users = cursor.fetchall()
             for user in users:
 
-                lst_grants = self.__list_grants(user[0], user[1])
+                lst_grants = self.__list_grants(host, user[0], user[1])
 
                 dict_users[user[0] + "@" + user[1]] = lst_grants
 
         dict_users = OrderedDict(sorted(dict_users.items()))
         return dict_users
 
-    def __list_grants(self, user, host):
+    def __list_grants(self, host, username, hostname):
         lst_permissions = list()
 
-        query_grants = f"SHOW GRANTS FOR '{user}'@'{host}'"
+        query_grants = f"SHOW GRANTS FOR '{username}'@'{hostname}'"
         self._logger.debug(query_grants)
 
         regex_grant_permissions = r"GRANT ([`%@A-Za-z0-9_,\s\(\)]+?)\s(ON|TO)"
         regex_grant_on = r"ON ([`\*\.a-z0-9-_]+)"
         regex_grant_options = r"(WITH GRANT OPTION)"
 
-        with self.db.cursor() as cursor:
+        conn = self.__connect(host)
+
+        with conn.cursor() as cursor:
             cursor.execute(query_grants)
 
             grants = cursor.fetchall()
@@ -69,27 +79,31 @@ class MySQL(Platform):
         lst_permissions_sorted = sorted(lst_permissions, key=lambda item: item["target"])
         return lst_permissions_sorted
 
-    def __list_databases(self):
-        db_ignore = ['information_schema', 'performance_schema', 'mysql', 'sys']
+    def __list_databases(self, host):
+        db_ignore = ['information_schema', 'performance_schema', 'mysql', 'sys', 'innodb', 'tmp']
 
         dict_databases = dict()
 
+        db_conn = self.__connect(host)
+
         show_db_query = "SHOW DATABASES"
-        with self.db.cursor() as cursor:
+        with db_conn.cursor() as cursor:
             cursor.execute(show_db_query)
 
             dbs = cursor.fetchall()
             for db in dbs:
                 if db[0] not in db_ignore:
-                    dict_databases[db[0]] = self.__list_tables(db[0])
+                    dict_databases[db[0]] = self.__list_tables(host, db[0])
 
             return dict_databases
 
-    def __list_tables(self, database):
+    def __list_tables(self, host, database):
         lst_tables = list()
 
+        db_conn = self.__connect(host)
+
         show_db_query = f"SHOW TABLES FROM `{database}`"
-        with self.db.cursor() as cursor:
+        with db_conn.cursor() as cursor:
             cursor.execute(show_db_query)
 
             tables = cursor.fetchall()
@@ -98,7 +112,7 @@ class MySQL(Platform):
 
             return lst_tables
 
-    def __markdown_users(self, dict_users):
+    def __markdown_users(self, host, dict_users):
         lst_content = list()
 
         lst_content.append(">[!info] General information")
@@ -117,10 +131,10 @@ class MySQL(Platform):
 
                 lst_content.append(f"| `{permit['target']}` | `{permit['permissions']}` | {options} |")
 
-        file = "mysql/users.md"
+        file = f"mysql/{host}/users.md"
         return {file: lst_content}
 
-    def __markdown_databases(self, dict_databases):
+    def __markdown_databases(self, host, dict_databases):
         lst_content = list()
 
         lst_content.append(">[!info] General information")
@@ -135,16 +149,20 @@ class MySQL(Platform):
 
             lst_content.append("")
 
-        file = "mysql/databases.md"
+        file = f"mysql/{host}/databases.md"
         return {file: lst_content}
 
     def _build_content(self):
         md_main = dict()
 
-        user_inventory = self.__list_users()
-        md_main.update(self.__markdown_users(user_inventory))
+        for host in self.__config:
 
-        db_inventory = self.__list_databases()
-        md_main.update(self.__markdown_databases(db_inventory))
+            self._logger.debug(f'- Host {host}')
+
+            user_inventory = self.__list_users(host)
+            md_main.update(self.__markdown_users(host, user_inventory))
+
+            db_inventory = self.__list_databases(host)
+            md_main.update(self.__markdown_databases(host, db_inventory))
 
         return md_main
