f91b4228f0cd14b7c7fb1cc95d164fe1fa4f46da
yakob-aleksandrovich <jakobaleksandrovich@gmail.com>
2026-02-10 15:44:44 +0100

    Cloudflare Workers are now also inventoryzed.

--- platforms/Cloudflare.py
+++ platforms/Cloudflare.py
@@ -1,5 +1,3 @@
-from github.Consts import mediaTypeReactionsPreview
-
 from .Platform import Platform
 from cloudflare import Cloudflare as CF
 from pprint import pp
@@ -66,6 +64,44 @@ class Cloudflare(Platform):
 
     return dict_return
 
+  def __enumerate_workers(self):
+    dict_return = dict()
+    dict_return['meta'] = dict()
+    dict_return['content'] = list()
+
+    # Workers
+    workers =  json.loads(self.__cf_agent.workers.scripts.list(account_id=self.__cf_account_id).model_dump_json())['result']
+    self._inc_api_call()
+
+    # Wrap it all up and return
+    dict_return['meta']['worker_count'] = len(workers)
+
+    # Domains related to workers
+    domains = json.loads(self.__cf_agent.workers.domains.list(account_id=self.__cf_account_id).model_dump_json())['result']
+    self._inc_api_call()
+
+    worker_field_filter = ['id', 'created_on', 'has_modules', 'modified_on']
+    for worker in workers:
+      dict_worker = self._filter_fields(worker, worker_field_filter)
+
+      # Get deployments
+      deployments = json.loads(self.__cf_agent.workers.scripts.deployments.get(script_name=worker['id'], account_id=self.__cf_account_id).model_dump_json())['deployments']
+      self._inc_api_call()
+
+      if len(deployments) > 0:
+        dict_worker['last_deployment'] = deployments[0]
+
+      # Link domain, if available
+      domain_list = [domain['hostname'] for domain in domains if domain['service'] == worker['id']]
+      if len(domain_list) == 1:
+        dict_worker['domain'] = domain_list[0]
+
+      dict_return['content'].append(dict_worker)
+
+    # Return
+    return dict_return
+
+
   def __enumerate_r2_buckets(self):
     dict_return = dict()
     dict_return['meta'] = dict()
@@ -101,6 +137,31 @@ class Cloudflare(Platform):
 
     return dict_map[code]
 
+  def __markdown_workers(self, workers):
+    lst_content = list()
+
+    lst_content.append(">[!info] General information")
+    lst_content.append(self._item('Workers', workers['meta']['worker_count']))
+    lst_content.append('')
+
+    for worker in workers['content']:
+      lst_content.append(f'### {worker['id']}')
+
+      if 'domain' in worker:
+        lst_content.append(self._item('Domain', worker['domain']))
+
+      lst_content.append(self._item('Created', self._format_date(worker['created_on'])))
+      lst_content.append(self._item('Modified', self._format_date(worker['modified_on'])))
+      lst_content.append(self._item('Has modules', worker['has_modules']))
+
+      if 'last_deployment' in worker:
+        last_deploy_date = self._format_date(worker['last_deployment']['created_on'])
+        deploy_id = worker['last_deployment']['versions'][0]['version_id'][0:8]
+        lst_content.append(self._item('Last deployment', f'{last_deploy_date} (`{deploy_id}`)'))
+
+    page = 'cloudflare/workers.md'
+    return {page: lst_content}
+
   def __markdown_r2_buckets(self, buckets):
     lst_content = list()
 
@@ -109,7 +170,7 @@ class Cloudflare(Platform):
     lst_content.append('')
 
     for bucket in buckets['content']:
-      lst_content.append(f'- `{bucket['name']}`')
+      lst_content.append(f'### {bucket['name']}')
       lst_content.append(self._item('Created', self._format_date(bucket['creation_date'])))
       lst_content.append(self._item('Storage class', bucket['storage_class']))
       lst_content.append(self._item('Location', f'{self.__code_to_location(bucket['location'])} ({bucket['location']})'))
@@ -144,13 +205,14 @@ class Cloudflare(Platform):
       lst_content.append(self._item('Latest deployment', f'{last_deployed} ({deploy_status})'))
 
       # Deployment source
-      deploy_source = project['latest_deployment']['source']
-      if deploy_source['type'] == 'github':
-        source_url = (f'https://github.com/{deploy_source['config']['owner']}/{deploy_source['config']['repo_name']} '
-                      f'(`{deploy_source['config']['production_branch']}`)')
-      else:
-        source_url = ''
-      lst_content.append(self._item('Deployed from', source_url))
+      if 'source' in project['latest_deployment']:
+          deploy_source = project['latest_deployment']['source']
+          if deploy_source['type'] == 'github':
+            source_url = (f'https://github.com/{deploy_source['config']['owner']}/{deploy_source['config']['repo_name']} '
+                          f'(`{deploy_source['config']['production_branch']}`)')
+          else:
+            source_url = 'unknown'
+          lst_content.append(self._item('Deployed from', source_url))
 
     page = 'cloudflare/pages.md'
     return {page: lst_content}
@@ -200,6 +262,9 @@ class Cloudflare(Platform):
 
     page_projects = self.__enumerate_page_projects()
     md_main.update(self.__markdown_page_projects(page_projects))
+
+    workers = self.__enumerate_workers()
+    md_main.update(self.__markdown_workers(workers))
 
     r2_buckets = self.__enumerate_r2_buckets()
     md_main.update(self.__markdown_r2_buckets(r2_buckets))
